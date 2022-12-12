8610b918830bf4925faf396dc2dd3b0020819176
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2022-12-11 22:05:14 -0500

    added packages, README prompts and init function

--- js/index.js
+++ js/index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is your project title?',
+          },
+          {
+            type: 'input',
+            name: 'Description',
+            message: 'Give a brief description of your project. (i.e. objective, motivation, what you learned etc..)',
+          },
+          {
+            type: 'input',
+            name: 'Installation',
+            message: 'Are there any Installation guidelines for your project?',
+          },
+          {
+            type: 'input',
+            name: 'Usage',
+            message: 'How do you use what was created in this project?',
+          },
+          {
+            type: 'input',
+            name: 'Contribution',
+            message: 'Are there any contributors or collaborators to your project? (include tutorials)',
+          },
+          {
+            type: 'input',
+            name: 'Email',
+            message: 'Contact Information for questions - insert email',
+          },
+          {
+            type: 'input',
+            name: 'Github',
+            message: 'What is your github repo name?',
+          },
+          {
+            type: 'list',
+            name: 'License',
+            message: 'What license are your using?',
+            choices: ['MIT', 'ISC', 'GNUPLv3'],
+            filter(val){
+                return val.toLowerCase();
+            }
+          },
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fileName = 'README.md'
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+)};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers)
+        return writeToFile(answers);
+    })
+    .catch((error)=> {
+        console.log(error)
+    })
+};
 
 // Function call to initialize app
 init();
